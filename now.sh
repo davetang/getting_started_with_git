9c3f973283bb46049c5a258d22caef8ec704714a
Dave Tang <davetingpongtang@gmail.com>
2022-03-24 10:28:40 +0900

    echo sleep time

--- now.sh
+++ now.sh
@@ -6,8 +6,12 @@ now(){
    date '+%Y/%m/%d %H:%M:%S'
 }
 
+# seconds to sleep
+s=2
+
 >&2 printf "[ %s %s ] Start\n" $(now)
-sleep 2
+>&2 echo Sleeping for ${s} seconds
+sleep ${s}
 >&2 printf "[ %s %s ] End\n" $(now)
 
 exit 0
