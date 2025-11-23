97bba80b7dabd8ccbda29924ef5e6220f07d983d
Dave Tang <davetingpongtang@gmail.com>
2025-11-23 22:20:18 +0900

    Add divide function

--- bisect_demo/calculator.py
+++ bisect_demo/calculator.py
@@ -9,6 +9,10 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    return a / b
+
 print("2 + 3 =", add(2, 3))
 print("10 - 4 =", subtract(10, 4))
 print("6 * 7 =", multiply(6, 7))
+print("20 / 4 =", divide(20, 4))
