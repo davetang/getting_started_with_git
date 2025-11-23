91dcb69b4ac78a438eeec78a1824c023dcd37ada
Dave Tang <davetingpongtang@gmail.com>
2025-11-23 22:24:33 +0900

    Add cube function

--- bisect_demo/calculator.py
+++ bisect_demo/calculator.py
@@ -18,9 +18,13 @@ def power(a, b):
 def square(a):
     return a * a
 
+def cube(a):
+    return a * a * a
+
 print("2 + 3 =", add(2, 3))
 print("10 - 4 =", subtract(10, 4))
 print("6 * 7 =", multiply(6, 7))
 print("20 / 4 =", divide(20, 4))
 print("2 ^ 8 =", power(2, 8))
 print("5 squared =", square(5))
+print("3 cubed =", cube(3))
