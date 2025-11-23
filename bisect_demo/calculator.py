f41e92ee0b3d141265612b850556aa137f807657
Dave Tang <davetingpongtang@gmail.com>
2025-11-23 22:18:53 +0900

    Simple calculator

---
+++ bisect_demo/calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+print("2 + 3 =", add(2, 3))
+print("10 - 4 =", subtract(10, 4))
+print("6 * 7 =", multiply(6, 7))
