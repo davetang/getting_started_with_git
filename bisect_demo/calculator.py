12a7ea9b05d457f66efbf050bb3ca81b352347e7
Dave Tang <davetingpongtang@gmail.com>
2025-11-23 22:22:02 +0900

    Add power function

--- bisect_demo/calculator.py
+++ bisect_demo/calculator.py
@@ -12,7 +12,11 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
+def power(a, b):
+    return a ** b
+
 print("2 + 3 =", add(2, 3))
 print("10 - 4 =", subtract(10, 4))
 print("6 * 7 =", multiply(6, 7))
 print("20 / 4 =", divide(20, 4))
+print("2 ^ 8 =", power(2, 8))
